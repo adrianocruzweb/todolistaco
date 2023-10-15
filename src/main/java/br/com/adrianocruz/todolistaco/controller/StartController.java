37208a23ed864577fc21ed2fe800974b06194d9c
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-10-14 20:56:55 -0400

    http request mapping starts with a route

--- src/main/java/br/com/adrianocruz/todolistaco/controller/StartController.java
+++ src/main/java/br/com/adrianocruz/todolistaco/controller/StartController.java
@@ -1,11 +1,15 @@
 package br.com.adrianocruz.todolistaco.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
+@RequestMapping("/firstRoute")
 public class StartController {
+    @GetMapping("/")
     public String firstMessage(){
         return "it works";
     }
